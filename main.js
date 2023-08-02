cd5a2226d27d5cf8ec3798afc080a7df86e4a618
Tanguy Villiot <Tanguy@ultiplace.com>
2023-08-02 15:39:41 +0200

    add gestionnary user #3

--- main.js
+++ main.js
@@ -13,73 +13,90 @@ const connectedUsers = new Set();
 let keepAliveId;
 
 const wss =
-  process.env.NODE_ENV === "production"
-    ? new WebSocket.Server({ server })
-    : new WebSocket.Server({ port: 5001 });
+    process.env.NODE_ENV === "production"
+        ? new WebSocket.Server({server})
+        : new WebSocket.Server({port: 5001});
 
 server.listen(serverPort);
 console.log(`Server started on port ${serverPort} in stage ${process.env.NODE_ENV}`);
 
 wss.on("connection", function (ws, req) {
-  console.log("Connection Opened");
-  console.log("Client size: ", wss.clients.size);
+    console.log("Connection Opened");
+    console.log("Client size: ", wss.clients.size);
 
-  connectedUsers.add(ws);
-
-  if (wss.clients.size === 1) {
-    console.log("first connection. starting keepalive");
-    keepServerAlive();
-  }
-
-  ws.on("message", (data) => {
-    let stringifiedData = data.toString();
-    if (stringifiedData === 'pong') {
-      console.log('keepAlive');
-      return;
+    if (wss.clients.size === 1) {
+        console.log("first connection. starting keepalive");
+        keepServerAlive();
     }
-    broadcast(ws, stringifiedData, false);
-  });
 
-  ws.on("close", (data) => {
-    console.log("closing connection");
+    ws.on("message", (data) => {
 
-    connectedUsers.delete(ws);
+      const message = data.toString();
 
-    if (wss.clients.size === 0) {
-      console.log("last client disconnected, stopping keepAlive interval");
-      clearInterval(keepAliveId);
-    }
-  });
+        if (message.startsWith("username:")) {
+            const username = message.replace("username:", "").trim();
+            console.log("Received username: ", username);
+
+            // Associate the username with the WebSocket connection
+            ws.username = username;
+
+            // Add the WebSocket connection to the set of connected users
+            connectedUsers.add(ws);
+
+            if (wss.clients.size === 1) {
+                console.log("first connection. starting keepalive");
+                keepServerAlive();
+            }
+        } else {
+            let stringifiedData = data.toString();
+            if (stringifiedData === 'pong') {
+                console.log('keepAlive');
+                return;
+            }
+            broadcast(ws, stringifiedData, false);
+        }
+    });
+
+    ws.on("close", (data) => {
+        console.log("closing connection");
+
+        connectedUsers.delete(ws);
+
+        if (wss.clients.size === 0) {
+            console.log("last client disconnected, stopping keepAlive interval");
+            clearInterval(keepAliveId);
+        }
+    });
 });
 
 // Implement broadcast function because of ws doesn't have it
 const broadcast = (ws, message, includeSelf) => {
-  if (includeSelf) {
-    wss.clients.forEach((client) => {
-      if (client.readyState === WebSocket.OPEN) {
-        client.send(message);
-      }
-    });
-  } else {
-    wss.clients.forEach((client) => {
-      if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(message);
-      }
-    });
-  }
+    if (includeSelf) {
+        wss.clients.forEach((client) => {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(message);
+            }
+        });
+    } else {
+        wss.clients.forEach((client) => {
+            if (client !== ws && client.readyState === WebSocket.OPEN) {
+                client.send(message);
+            }
+        });
+    }
 };
 
 /**
  * Sends a ping message to all connected clients every 50 seconds
  */
- const keepServerAlive = () => {
-  keepAliveId = setInterval(() => {
-    wss.clients.forEach((client) => {
-      if (client.readyState === WebSocket.OPEN) {
-        client.send('ping');
-      }
-    });
-  }, 50000);
+const keepServerAlive = () => {
+    keepAliveId = setInterval(() => {
+        wss.clients.forEach((client) => {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send('ping');
+            }
+        });
+    }, 50000);
 };
 
 
@@ -89,7 +106,7 @@ app.get('/', (req, res) => {
 
 // Route API pour obtenir les utilisateurs connectés
 app.get('/api/connected-users', (req, res) => {
-  // Convertir la liste des utilisateurs connectés en tableau et renvoyer en JSON
-  const users = [...connectedUsers].map(user => user); // Supposons que l'utilisateur ait une propriété "id"
-  res.json({ users });
+    // Convertir la liste des utilisateurs connectés en tableau et renvoyer en JSON
+    const users = [...connectedUsers].map(user => user); // Supposons que l'utilisateur ait une propriété "id"
+    res.json({users});
 });
